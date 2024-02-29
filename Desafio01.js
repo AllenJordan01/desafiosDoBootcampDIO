91b4d85ee6a0ab1cd085afee88d5a517158ac6fa
Allen Jordan <allenjordan65@gmail.com>
2024-02-29 15:36:05 -0300

    Resolução do Desafio 01 - classificador de nível de herói

---
+++ Desafio01.js
@@ -0,0 +1,56 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+ input: process.stdin,
+ output: process.stdout
+});
+
+let continuar = true;
+
+const classificarNivelDeHeroi = () => {
+ rl.question("Digite o nome do herói: ", (heroName) => {
+    rl.question("Digite o XP do herói: ", (heroXP) => {
+      let heroLeague;
+
+      if (heroXP <= 1000){
+          heroLeague = "Ferro";
+      }
+      else if (heroXP <= 2000){
+          heroLeague = "Bronze";
+      }
+      else if (heroXP <= 5000){
+          heroLeague = "Prata";
+      }
+      else if (heroXP <= 7000){
+          heroLeague = "Ouro";
+      }
+      else if (heroXP <= 8000){
+          heroLeague = "Platina";
+      }
+      else if (heroXP <= 9000){
+          heroLeague = "Ascendente";
+      }
+      else if (heroXP <= 10000){
+          heroLeague = "Imortal";
+      }
+      else {
+          heroLeague = "Radiante";
+      }
+
+      console.log(`O Herói de nome ${heroName}, está no nível de ${heroLeague}`);
+
+      rl.question("Gostaria de parar? S - sim || N - não\n", (parar) => {
+        if (parar.toLowerCase() == "s"){
+          continuar = false;
+        }
+        if (continuar) {
+          solicitarNomeHeroi();
+        } else {
+          rl.close();
+        }
+      });
+    });
+ });
+};
+
+classificarNivelDeHeroi();
